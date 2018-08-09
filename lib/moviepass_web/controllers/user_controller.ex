467d94d5ff4b2d6a35754bd522d2d5d73206d189
Johann Kerr <johannkerr@gmail.com>
2018-08-09 01:57:16 -0400

    added basic plug

--- lib/moviepass_web/controllers/user_controller.ex
+++ lib/moviepass_web/controllers/user_controller.ex
@@ -1,11 +1,12 @@
 defmodule MoviepassWeb.UserController do
   use MoviepassWeb, :controller
+  plug :authenticate when action in [:index]
   alias Moviepass.Accounts
   alias Moviepass.Accounts.User
 
   def index(conn, _params) do
-    users = Accounts.list_users
-    render conn, "index.html", users: users
+    users = Accounts.list_users()
+    render(conn, "index.html", users: users)
   end
 
   def new(conn, _params) do
@@ -17,10 +18,22 @@ defmodule MoviepassWeb.UserController do
     case Accounts.register_user(user_params) do
       {:ok, user} ->
         conn
+        |> MoviepassWeb.Auth.login(user)
         |> put_flash(:info, "#{user.name} created!")
         |> redirect(to: Routes.user_path(conn, :index))
       {:error, %Ecto.Changeset{} = changeset} ->
         render(conn, "new.html", changeset: changeset)
+    end
+  end
+
+  defp authenticate(conn, _opts) do
+    if conn.assigns.current_user do
+      conn
+    else
+      conn
+      |> put_flash(:error, "You must be logged in to access that page")
+      |> redirect(to: Routes.page_path(conn, :index))
+      |> halt()
     end
   end
 end
