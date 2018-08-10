43b59bf285b2d644f09db37d0e120d416eb62297
Johann Kerr <johannkerr@gmail.com>
2018-08-10 09:58:24 -0400

    remove keys

--- lib/moviepass_web/oauth/learn_client.ex
+++ lib/moviepass_web/oauth/learn_client.ex
@@ -1,7 +1,7 @@
-# Thanks Team Hot Sauce
+Team Hot Sauce
 defmodule MoviepassWeb.Oauth.LearnClient do
-  @learn_client_id "5e6b6aca3ab5a3f3e3ab23ed5d749bc2d3abbd1da24dc239d9fa9985ff177fd5"
-  @learn_client_secret "f9524f0f1ddc42c6bf2394e1ff473825eb975dab44ccdf5031047fcc54a5603f"
+  @learn_client_id "SUPER SECRET CLIENT ID"
+  @learn_client_secret "SUPER SECRET SECRET"
   @host_base_url "http://localhost:4000"
   @redirect_uri "#{@host_base_url}/oauth/learn/callback"
   @learn_url "https://learn.co"
